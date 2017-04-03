051d886ba8bd568485e507919b703068ab598694
Xinyang <xinyzhang9@gmail.com>
2017-04-03 16:56:01 -0700

    make a list

--- egg.js
+++ egg.js
@@ -2,6 +2,24 @@ var card = new Vue({
 	el: '#card',
 	data: {
 		title: 'Dinosaurs',
-		content: '<strong>Dinosaurs</strong> are a diverse group of animals of the clade <em>Dinosauria</em> that first appeared during the Triassic period.'
+		items: [
+			{ text: 'Velociraptor' },
+			{ text: 'Triceratops' },
+			{ text: 'Stegosaurus' }
+		]
+	},
+	methods:{
+		addItem: function(){
+			var input = document.getElementById('itemForm');
+			if(input.value !== ''){
+				this.items.push({
+					text: input.value
+				});
+				input.value = '';
+			}
+		},
+		deleteItem: function(index){
+			this.items.splice(index,1);
+		}
 	}
 })
