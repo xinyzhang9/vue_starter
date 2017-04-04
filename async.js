8c9c569e59ffc1a88ad3282cae066df7bdec8b73
Xinyang <xinyzhang9@gmail.com>
2017-04-04 15:38:52 -0700

    async and style binding

---
+++ async.js
@@ -0,0 +1,31 @@
+var card = new Vue({
+	el: '#card',
+	data: {
+		title: 'Dinosaurs',
+		input: '',
+		buttonText: 'Add Dinosaur',
+		items: [
+			'Tyrannosaurus',
+			'Triceratops',
+			'Stegosaurus'
+		]
+	},
+	computed: {
+		buttonDisabled: function(){
+			return this.input == '';
+		}
+	},
+	watch: {
+		input: _.debounce(function(){
+			this.buttonText = this.input !== '' ? 'Add ' + this.input : 'Add Dinosaur';
+		},250)
+	},
+	methods: {
+		addItem: function(){
+			if(this.input !== ''){
+				this.items.push( this.input );
+				this.input = '';
+			}
+		}
+	}
+});
