9a5cb96d977510f052d0b6e5131b965089485b8c
Xinyang <xinyzhang9@gmail.com>
2017-04-03 17:19:12 -0700

    filters

--- egg.js
+++ egg.js
@@ -2,24 +2,36 @@ var card = new Vue({
 	el: '#card',
 	data: {
 		title: 'Dinosaurs',
-		items: [
-			{ text: 'Velociraptor' },
-			{ text: 'Triceratops' },
-			{ text: 'Stegosaurus' }
+		dinos: [
+			{ 
+				text: 'Velociraptor',
+				weight: '15 kg'
+			},
+			{ 
+				text: 'triceratops',
+				weight: '6.000 kg'
+			},
+			{ 
+				text: 'Stegosaurus',
+				weight: '2,500 kg'
+			}
 		]
 	},
-	methods:{
-		addItem: function(){
-			var input = document.getElementById('itemForm');
-			if(input.value !== ''){
-				this.items.push({
-					text: input.value
-				});
-				input.value = '';
-			}
+	filters: {
+		capitalize: function(value){
+			if(!value) return '';
+			value = value.toString();
+			return value.charAt(0).toUpperCase()+value.slice(1);
 		},
-		deleteItem: function(index){
-			this.items.splice(index,1);
+		undercase: function(value){
+			if(!value) return '';
+			value = value.toString();
+			return value.toLowerCase();
+		},
+		url: function(value){
+			if(!value) return '';
+			value = value.toString();
+			return 'https://en.wikipedia.org/wiki/'+value
 		}
 	}
 })
